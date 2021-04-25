6672ee4fa9ea7dd84a0bc4c54b9fbb1bd6719674
18867375028 <46912873+18867375028@users.noreply.github.com>
2021-04-25 16:37:37 +0800

    Update Reading_brother_cookie.js

--- Rewrite/Reading_brother_cookie.js
+++ Rewrite/Reading_brother_cookie.js
@@ -1,5 +1,6 @@
 #番茄阅读  m.*
 ^http://m.*./reada/getTask url script-request-header https://raw.githubusercontent.com/age174/-/main/fqkk.js
+
 hostname = m.*
 #云扫码
 ^http://.+?[^/]/yunonline/v\d+/redirect/(?!undefined) url script-request-header https://raw.githubusercontent.com/age174/-/main/ysm.js
