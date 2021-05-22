3adf561bdcbdd16a1a313a08b31a6f7b3b58ae75
旺仔 <46912873+GoodStudying@users.noreply.github.com>
2021-05-22 23:54:38 +0800

    Update jd_dreamFactory.js

--- task/v2p_task/jd_dreamFactory.js
+++ task/v2p_task/jd_dreamFactory.js
@@ -43,8 +43,8 @@ let tuanActiveId = `mNGPTrOPluOSeZcupPI40w==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
 const inviteCodes = [
-  'RhXtja-WxzNcFDL5eeOiag==',
-  "RhXtja-WxzNcFDL5eeOiag==",
+  'RhXtja-WxzNcFDL5eeOiag==@7ieSejtFM5V7FuECQMyS9Q==',
+  "RhXtja-WxzNcFDL5eeOiag==@7ieSejtFM5V7FuECQMyS9Q==",
   'RhXtja-WxzNcFDL5eeOiag==',
   'RhXtja-WxzNcFDL5eeOiag=='
 ];
